041db2f83a1ff97014483af1735dd90fb91b2992
Mark Eschbach <meschbach@gmail.com>
2022-08-23 12:39:28 -0700

    client/refactor: pulls KindBuilder into own file

--- pkg/v1/query.go
+++ pkg/v1/query.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"context"
-	"encoding/json"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -69,61 +68,6 @@ func (q *QueryBuilder) Perform(ctx context.Context) (QueryResults, error) {
 	}
 
 	return &wireResultInterpreter{results: result}, err
-}
-
-type KindBuilder struct {
-	kind  string
-	eq    []equalityPredicate
-	match json.RawMessage
-	on    OnStreamQueryResult
-}
-
-func (k *KindBuilder) Match(example interface{}) *KindBuilder {
-	serialized, err := json.Marshal(example)
-	if err != nil {
-		panic(err)
-	}
-	k.match = serialized
-	return k
-}
-
-// On registers handler to be invoked when streaming results.  If invoked multiple times the last invocation will be called.
-func (k *KindBuilder) On(handler OnStreamQueryResult) *KindBuilder {
-	k.on = handler
-	return k
-}
-
-func (k *KindBuilder) Eq(property string, value string) *KindBuilder {
-	return k.Equals([]string{property}, value)
-}
-
-func (k *KindBuilder) Equals(property []string, value string) *KindBuilder {
-	k.eq = append(k.eq, equalityPredicate{
-		Property: property,
-		Value:    value,
-	})
-	return k
-}
-
-func (k *KindBuilder) toKindConstraint() KindConstraint {
-	var matchers []WireMatcherV1
-	for _, m := range k.eq {
-		matchers = append(matchers, WireMatcherV1{
-			Property: m.Property,
-			Value:    []string{m.Value},
-		})
-	}
-	return KindConstraint{
-		Kind:        k.kind,
-		Eq:          matchers,
-		MatchSubset: k.match,
-	}
-}
-
-func (k *KindBuilder) postProcessing(p *postProcessingHandlers) {
-	if k.on != nil {
-		p.register(k.kind, k.on)
-	}
 }
 
 type equalityPredicate struct {
---
+++ pkg/v1/queryKindBuilder.go
@@ -0,0 +1,59 @@
+package v1
+
+import "encoding/json"
+
+// todo: move to queryKindBuilder
+type KindBuilder struct {
+	kind  string
+	eq    []equalityPredicate
+	match json.RawMessage
+	on    OnStreamQueryResult
+}
+
+func (k *KindBuilder) Match(example interface{}) *KindBuilder {
+	serialized, err := json.Marshal(example)
+	if err != nil {
+		panic(err)
+	}
+	k.match = serialized
+	return k
+}
+
+// On registers handler to be invoked when streaming results.  If invoked multiple times the last invocation will be called.
+func (k *KindBuilder) On(handler OnStreamQueryResult) *KindBuilder {
+	k.on = handler
+	return k
+}
+
+func (k *KindBuilder) Eq(property string, value string) *KindBuilder {
+	return k.Equals([]string{property}, value)
+}
+
+func (k *KindBuilder) Equals(property []string, value string) *KindBuilder {
+	k.eq = append(k.eq, equalityPredicate{
+		Property: property,
+		Value:    value,
+	})
+	return k
+}
+
+func (k *KindBuilder) toKindConstraint() KindConstraint {
+	var matchers []WireMatcherV1
+	for _, m := range k.eq {
+		matchers = append(matchers, WireMatcherV1{
+			Property: m.Property,
+			Value:    []string{m.Value},
+		})
+	}
+	return KindConstraint{
+		Kind:        k.kind,
+		Eq:          matchers,
+		MatchSubset: k.match,
+	}
+}
+
+func (k *KindBuilder) postProcessing(p *postProcessingHandlers) {
+	if k.on != nil {
+		p.register(k.kind, k.on)
+	}
+}

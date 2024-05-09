33793f73c4d0062cb9e27fae895233ba7c8c9342
Mark Eschbach <meschbach@gmail.com>
2024-05-09 09:07:29 -0700

    tests/integration: test gRPC when testing other connectors

--- integration-tests.sh
+++ integration-tests.sh
@@ -18,7 +18,7 @@ export PGCQRS_TEST_URL="http://localhost:9000"
 export PGCQRS_TEST_APP_BASE="systest-"
 go test -count=1 --timeout 5s ./systest/...
 
-#export PGCQRS_TEST_TRANSPORT="grpc"
-#export PGCQRS_TEST_URL="localhost:9001"
-#export PGCQRS_TEST_APP_BASE="systest-"
-#go test -count=1 --timeout 5s ./systest/...
+export PGCQRS_TEST_TRANSPORT="grpc"
+export PGCQRS_TEST_URL="localhost:9001"
+export PGCQRS_TEST_APP_BASE="systest-"
+go test -count=1 --timeout 5s ./systest/...

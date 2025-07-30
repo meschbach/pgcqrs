1017eeb779aa02c83bc39a3181c39b9918363e90
Mark Eschbach <meschbach@gmail.com>
2025-07-30 11:14:30 -0700

    rest/error: mark the service span as having errored when the client errors

--- internal/junk/restful/clienterror.go
+++ internal/junk/restful/clienterror.go
@@ -2,6 +2,7 @@ package restful
 
 import (
 	"fmt"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 	"net/http"
 )
@@ -9,6 +10,7 @@ import (
 func ClientError(writer http.ResponseWriter, request *http.Request, problem error) {
 	ctx := request.Context()
 	span := trace.SpanFromContext(ctx)
+	span.SetStatus(codes.Error, problem.Error())
 	span.RecordError(problem)
 	if debug {
 		fmt.Printf("Error:\n%s\n", problem.Error())
